6d4f5bee3a798bf97fdb68d92bc911650d9807b4
jaclynpalmer <jaclyn.palmer@byu.edu>
2023-09-30 12:06:32 -0600

    updated deploy script

--- jfloral-html/deployFiles.sh
+++ jfloral-html/deployFiles.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-printf "is this freaking working\n"
-while getopts k:h: flag
+
+while getopts k:h:s: flag
 do
     case "${flag}" in
         k) key=${OPTARG};;
         h) hostname=${OPTARG};;
+        s) service=${OPTARG};;
     esac
 done
 
-if [[ -z "$key" || -z "$hostname" ]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname>\n\n"
+    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
-file
-printf "\n----> Deploying files to $hostname with $key\n"
+fi
+
+printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
-rm -rf services/public
-mkdir -p services/public
+rm -rf services/${service}/public
+mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/public
+scp -r -i "$key" * ubuntu@$hostname:services/$service/public
