b1bfeb83a28109433a7320f0bf383b2b7a655075
jaclynpalmer <jaclyn.palmer@byu.edu>
2023-12-07 18:12:41 -0700

    added function to db

---
+++ jfloral-db/database.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const estimateCollection = db.collection('estimates');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+async function addEstimate(estimate){
+    const result = await estimateCollection.insertOne(estimate);
+  return result;
+}
+
+
+
