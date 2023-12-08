ddf981136b1f7485e0e0a162e40de0c1bffb9c06
jaclynpalmer <jaclyn.palmer@byu.edu>
2023-12-07 20:57:39 -0700

    fixed conneciton to db

--- jfloral-service/database.js
+++ jfloral-service/database.js
@@ -3,8 +3,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('startup');
-const estimateCollection = db.collection('estimates');
+const db = client.db('estimates');
+const estimateCollection = db.collection('totalCost');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
