74189d326773f0f7dc43f6a68dd5c841d7d4839b
jaclynpalmer <jaclyn.palmer@byu.edu>
2023-12-08 23:13:32 -0700

    debugging mongo

--- jfloral-service/database.js
+++ jfloral-service/database.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
-
+async function main(){
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('estimates');
 const estimateCollection = db.collection('totalCost');
+
+// const db = client.db('rental');
+//   const collection = db.collection('house');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -15,10 +18,22 @@ const estimateCollection = db.collection('totalCost');
   process.exit(1);
 });
 
+
+const house = {
+  name: 'Beachfront views',
+  summary: 'From your bedroom to the beach, no shoes required',
+  property_type: 'Condo',
+  beds: 1,
+};
+
 async function addEstimate(estimate) {
-    const result = await estimateCollection.insertOne(estimate);
+      const result = await estimateCollection.insertOne(estimate);
   return result;
 }
+// await collection.insertOne(house);
+
 
 module.exports = { addEstimate };
 
+}
+main().catch(console.error);
